005b196a3329a3d68a6ba13ca878c93b2e571281
Benjy Cui <benjytrys@gmail.com>
2016-05-04 15:04:14 +0800

    chore: add index.html

--- packages/bisheng-core/lib/index.js
+++ packages/bisheng-core/lib/index.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
 const path = require('path');
 const dora = require('dora');
+const mkdirp = require('mkdirp');
 const getConfig = require('./utils/get-config');
 
 exports.start = function start(program) {
   const configFile = program.config || `${process.cwd()}/bisheng.config.js`;
   const config = getConfig(configFile);
+
+  mkdirp.sync(config.output);
+  fs.createReadStream(`${__dirname}${path.sep}template.html`)
+    .pipe(fs.createWriteStream(`${process.cwd()}${path.sep}index.html`));
 
   // TODO
   dora({
