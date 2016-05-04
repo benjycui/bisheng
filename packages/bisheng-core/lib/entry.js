99d159b50460aea62a9de70a0117bf9e90f252b2
Benjy Cui <benjytrys@gmail.com>
2016-05-04 15:35:10 +0800

    feat: basic support for theme

--- packages/bisheng-core/lib/entry.js
+++ packages/bisheng-core/lib/entry.js
@@ -1,17 +1,26 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const ReactRouter = require('react-router');
-
 const data = require('./loaders/bisheng.data');
+
+function templateWrapper(Template) {
+  return (props) => {
+    return React.createElement(
+      Template,
+      Object.assign({}, props, { data: data.markdown})
+    );
+  };
+}
+
 const theme = data.theme;
 const routes = Object.keys(theme.routes).map((path, index) => {
   return React.createElement(ReactRouter.Route, {
     key: index,
     path,
-    component: theme.routes[path],
+    component: templateWrapper(theme.routes[path]),
   });
 });
-console.log(routes);
+
 ReactDOM.render(
   React.createElement(ReactRouter.Router, {
     history: ReactRouter.browserHistory,
