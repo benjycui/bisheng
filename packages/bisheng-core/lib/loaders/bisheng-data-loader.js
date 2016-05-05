3a9333c610b2d1ebc2065f52bbca7e1e536491be
Benjy Cui <benjytrys@gmail.com>
2016-05-05 09:34:49 +0800

    feat: support plugin in browser

--- packages/bisheng-core/lib/loaders/bisheng-data-loader.js
+++ packages/bisheng-core/lib/loaders/bisheng-data-loader.js
@@ -15,11 +15,15 @@ module.exports = function bishengDataLoader(/* content */) {
 
   const markdownData = generateMarkdownData(config.source, config.extension);
   const plugins = resolvePlugins(config.plugins, true);
+  const pluginsString = plugins.map(
+    (plugin) =>
+      `require('${plugin[0]}')(${JSON.stringify(plugin[1])})`
+  ).join(',\n');
 
   return 'module.exports = {' +
     `\n  markdown: ${markdownData},` +
     `\n  theme: require('${themePath}'),` +
     `\n  notFound: require('${themePath}${path.sep}template${path.sep}NotFound'),` +
-    `\n  plugins: ${JSON.stringify(plugins, null, 2)},` +
+    `\n  plugins: [\n${pluginsString}\n],` +
     `\n};`;
 };
