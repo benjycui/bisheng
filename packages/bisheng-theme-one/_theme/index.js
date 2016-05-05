a778026c55dac4f068eb8b159657f14280f65332
Benjy Cui <benjytrys@gmail.com>
2016-05-05 16:10:53 +0800

    feat: add Layout

--- packages/bisheng-theme-one/_theme/index.js
+++ packages/bisheng-theme-one/_theme/index.js
@@ -1,7 +1,16 @@
 module.exports = {
+  home: '/',
+  sitename: 'One',
+  // tagline: 'BiSheng Theme',
+  // navigation: [{
+  //   title: 'BiSheng',
+  //   link: 'https://github.com/benjycui/bisheng',
+  // }],
+  footer: 'The one theme for bisheng which is used to transform Markdown files into a SPA website using React.',
+  // hideBisheng: true,
+  github: 'https://github.com/benjycui/bisheng-theme-one',
   routes: {
-    '/': require('./template/Home'),
-    '/archive': require('./template/Archive'),
+    '/': require('./template/Archive'),
     '/posts/:post': require('./template/Article'),
   }
 };
---
+++ packages/bisheng-theme-one/_theme/template/Layout.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router';
+import '../static/style';
+
+export default ({ config, children }) => {
+  return (
+    <div>
+      <div className="header">
+        <div className="container">
+          <div className="brand">
+            <Link className="home" to={config.home}>{config.sitename}</Link>
+            {
+              !config.tagline ? null :
+                <span>- <span className="tagline">{config.tagline}</span></span>
+            }
+          </div>
+          {
+            !config.navigation ? null :
+              <div className="menu" role="navigation">
+                {
+                  config.navigation.map((item, index) =>
+                    <Link to={item.link} key={index}>{item.title}</Link>
+                  )
+                }
+              </div>
+          }
+        </div>
+      </div>
+      <div className="document yue">
+        {children}
+      </div>
+      <div className="footer">
+        {config.footer ? config.footer : null}
+        {
+          config.hideBisheng ? null :
+            <p className="copyright">powered by <a href="https://github.com/benjycui/bisheng">BiSheng</a></p>
+        }
+      </div>
+      {
+        !config.github ? null :
+          <div className="github"><a className="github-link" href={config.github}>Fork me on GitHub</a></div>
+      }
+    </div>
+  );
+}
