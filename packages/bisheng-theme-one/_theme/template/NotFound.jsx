4a21d75b405e3c4897b68a5898287da8f65570a9
Benjy Cui <benjytrys@gmail.com>
2016-05-06 15:24:02 +0800

    feat: add NotFound page

--- packages/bisheng-theme-one/_theme/template/NotFound.jsx
+++ packages/bisheng-theme-one/_theme/template/NotFound.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import DocumentTitle from 'react-document-title';
+import Layout from './Layout';
 
-export default ({ data, utils }) => {
-  return <h1>404 Not Found!</h1>;
+export default (props) => {
+  return (
+    <DocumentTitle title="Not Found | BiSheng Theme One">
+      <Layout {...props}>
+        <h1 className="entry-title">404 Not Found!</h1>
+      </Layout>
+    </DocumentTitle>
+  );
 }
