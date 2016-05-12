c643f7dbdb11f9c80b49e21944101ee084ebecc4
Benjy Cui <benjytrys@gmail.com>
2016-05-12 17:26:54 +0800

    feat: finish description feature

--- packages/bisheng-plugin-description/lib/node.js
+++ packages/bisheng-plugin-description/lib/node.js
@@ -3,5 +3,16 @@
 const JsonML = require('jsonml.js/lib/utils');
 
 module.exports = (markdownData) => {
+  const content = markdownData.content;
+  const contentChildren = JsonML.getChildren(content);
+  const dividerIndex = contentChildren.findIndex((node) => JsonML.getTagName(node) === 'hr');
+
+  markdownData.description = ['section']
+    .concat(contentChildren.slice(0, dividerIndex));
+  markdownData.content = [
+    JsonML.getTagName(content),
+    JsonML.getAttributes(content) || {},
+  ].concat(contentChildren.slice(dividerIndex + 1));
+
   return markdownData;
 };
