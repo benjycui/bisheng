677e74f5ba69d6adc965ae810cb13aa617075294
Benjy Cui <benjytrys@gmail.com>
2016-05-13 09:52:49 +0800

    chore: add unit test stub

--- packages/bisheng-core/lib/utils/generate-files-path.js
+++ packages/bisheng-core/lib/utils/generate-files-path.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const R = require('ramda');
 const exist = require('exist.js');
 
---
+++ packages/bisheng-core/test/utils/generate-files-path.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const assert = require('assert');
+const generateFilesPath = require('../../lib/utils/generate-files-path');
+
+describe('utils/generate-files-path', function() {
+  it('should be OK', function() {
+    assert.ok(true);
+  });
+});
