a7a45b8d14b1e771478f067b8defcb54222093ff
Benjy Cui <benjytrys@gmail.com>
2016-05-24 11:30:33 +0800

    fix: should check isElement

--- packages/bisheng-core/lib/bisheng-plugin-highlight/lib/browser.js
+++ packages/bisheng-core/lib/bisheng-plugin-highlight/lib/browser.js
@@ -6,15 +6,18 @@ const JsonML = require('jsonml.js/lib/utils');
 module.exports = (/* config */) => {
   return {
     converters: [
-      [(node) => JsonML.getTagName(node) === 'pre', (node, index) => {
-        const attr = JsonML.getAttributes(node);
-        return React.createElement('pre', {
-          key: index,
-          className: `language-${attr.lang}`,
-        }, React.createElement('code', {
-          dangerouslySetInnerHTML: { __html: attr.highlighted },
-        }));
-      }],
+      [
+        (node) => JsonML.isElement(node) && JsonML.getTagName(node) === 'pre',
+        (node, index) => {
+          const attr = JsonML.getAttributes(node);
+          return React.createElement('pre', {
+            key: index,
+            className: `language-${attr.lang}`,
+          }, React.createElement('code', {
+            dangerouslySetInnerHTML: { __html: attr.highlighted },
+          }));
+        }
+      ],
     ],
   };
 };
