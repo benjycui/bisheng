b6ce8eaa5312cfe4323bfd2366924d61e8a63bf0
Benjy Cui <benjytrys@gmail.com>
2016-05-24 12:06:36 +0800

    bump 0.1.0-beta.0

---
+++ packages/bisheng-plugin-react/lib/config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const path = require('path');
+
+function generateQuery(config) {
+  return Object.keys(config)
+    .map((key) => `${key}=${config[key]}`)
+    .join('&');
+}
+
+module.exports = (config) => {
+  return {
+    webpackConfig(bishengWebpackConfig) {
+      bishengWebpackConfig.module.loaders.forEach((loader) => {
+        if (loader.test.toString() !== '/\\.md$/') return;
+        const babelIndex = loader.loaders.indexOf('babel');
+        const query = generateQuery(config);
+        loader.loaders.splice(babelIndex + 1, 0, path.join(process.cwd(), `jsonml-react-loader?${query}`));
+      });
+      return bishengWebpackConfig;
+    },
+  };
+};
