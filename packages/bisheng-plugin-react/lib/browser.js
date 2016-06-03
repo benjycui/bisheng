36045147fe4851ab0d6955b17fa78ea405e5bc2b
Benjy Cui <benjytrys@gmail.com>
2016-06-03 20:00:54 +0800

    style: to please lint

--- packages/bisheng-plugin-react/lib/browser.js
+++ packages/bisheng-plugin-react/lib/browser.js
@@ -1,12 +1,13 @@
 'use strict';
 
+/* eslint-disable no-var */
 var React = require('react');
 
 module.exports = function() {
   return {
     converters: [
       [
-        function(node) { return typeof node === 'function' },
+        function(node) { return typeof node === 'function'; },
         function(node, index) {
           return React.cloneElement(node(), { key: index });
         },
