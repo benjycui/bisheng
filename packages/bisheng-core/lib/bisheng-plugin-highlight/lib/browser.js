7b096142ab5ef50d48a1a0446cd029484d576c00
Benjy Cui <benjytrys@gmail.com>
2016-06-06 10:13:12 +0800

    fix: use var

--- packages/bisheng-core/lib/bisheng-plugin-highlight/lib/browser.js
+++ packages/bisheng-core/lib/bisheng-plugin-highlight/lib/browser.js
@@ -10,7 +10,7 @@ module.exports = function() {
       [
         function(node) { return JsonML.isElement(node) && JsonML.getTagName(node) === 'pre'; },
         function(node, index) {
-          const attr = JsonML.getAttributes(node);
+          var attr = JsonML.getAttributes(node);
           return React.createElement('pre', {
             key: index,
             className: 'language-' + attr.lang,
