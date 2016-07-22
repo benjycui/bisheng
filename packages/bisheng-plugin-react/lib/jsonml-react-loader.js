d568a9b0d68d587dadee86507140c268151ac69f
YouBao Nong <noyobo@gmail.com>
2016-07-22 09:27:45 +0800

    refactor: replace esprima with babylon (#1)

--- packages/bisheng-plugin-react/lib/jsonml-react-loader.js
+++ packages/bisheng-plugin-react/lib/jsonml-react-loader.js
@@ -1,18 +1,31 @@
 'use strict';
-const recast = require('recast');
-const builders = recast.types.builders;
+
 const loaderUtils = require('loader-utils');
+const babylon = require('babylon');
+const types = require('babel-types');
+const generator = require('babel-generator').default;
+const traverse = require('babel-traverse').default;
 
-// WAITING: esprima will support JSX
-const parseOptions = {
-  parser: require('esprima-fb'),
-};
-
-function isImport(node) {
-  return node.type === 'ImportDeclaration';
-}
-function isNotImport(node) {
-  return !isImport(node);
+function parser(content) {
+  return babylon.parse(content, {
+    sourceType: 'module',
+    plugins: [
+      'jsx',
+      'flow',
+      'asyncFunctions',
+      'classConstructorCall',
+      'doExpressions',
+      'trailingFunctionCommas',
+      'objectRestSpread',
+      'decorators',
+      'classProperties',
+      'exportExtensions',
+      'exponentiationOperator',
+      'asyncGenerators',
+      'functionBind',
+      'functionSent',
+    ],
+  });
 }
 
 module.exports = function jsonmlReactLoader(content) {
@@ -22,40 +35,72 @@ module.exports = function jsonmlReactLoader(content) {
 
   const query = loaderUtils.parseQuery(this.query);
   const lang = query.lang || 'react-example';
+  const noreact = query.noreact;
 
   let imports = [];
-  const ast = recast.visit(recast.parse(content, parseOptions), {
-    visitArrayExpression(path) {
-      const node = path.node;
 
+  const inputAst = parser(content);
+
+  traverse(inputAst, {
+    ArrayExpression: function(path) {
+      const node = path.node;
       const firstItem = node.elements[0];
       const secondItem = node.elements[1];
-
+      let renderReturn;
       if (firstItem &&
-          firstItem.type === 'Literal' &&
-          firstItem.value === 'pre' &&
-          secondItem.properties[0].value.value === lang) {
-        const codeAst = recast.parse(node.elements[2].elements[1].value, parseOptions);
+        firstItem.type === 'StringLiteral' &&
+        firstItem.value === 'pre' &&
+        secondItem.properties[0].value.value === lang) {
+        let codeNode = node.elements[2].elements[1];
+        let code = codeNode.value;
+
+        const codeAst = parser(code);
+
+        traverse(codeAst, {
+          ImportDeclaration: function(importPath) {
+            imports.push(importPath.node);
+            importPath.remove();
+          },
+          CallExpression: function(CallPath) {
+            const CallPathNode = CallPath.node;
+            if (CallPathNode.callee &&
+              CallPathNode.callee.object &&
+              CallPathNode.callee.object.name === 'ReactDOM' &&
+              CallPathNode.callee.property &&
+              CallPathNode.callee.property.name === 'render') {
+
+              renderReturn = types.returnStatement(
+                CallPathNode.arguments[0]
+              );
+
+              CallPath.remove();
+            }
+          },
+        });
+
         const astProgramBody = codeAst.program.body;
+        const codeBlock = types.BlockStatement(astProgramBody);
 
-        const codeImports = astProgramBody.filter(isImport);
-        imports = imports.concat(codeImports);
+        // ReactDOM.render always at the last of preview method
+        if (renderReturn) {
+          astProgramBody.push(renderReturn);
+        }
 
-        const codeBody = astProgramBody.filter(isNotImport);
-        const lastIndex = codeBody.length - 1;
-        codeBody[lastIndex] = builders.returnStatement(
-          codeBody[lastIndex].expression.arguments[0]
-        );
-
-        return builders.functionExpression(null, [], builders.blockStatement(codeBody));
+        const coceFunction = types.functionExpression(null, [], codeBlock);
+        path.replaceWith(coceFunction);
       }
-
-      this.traverse(path);
     },
   });
 
-  ast.program.body = imports.concat(ast.program.body);
+  for (let k = 0; k < imports.length; k++) {
+    inputAst.program.body.unshift(imports[k]);
+  }
+
+  const code = generator(inputAst, null, content).code;
+  if (noreact) {
+    return code;
+  }
   return 'import React from \'react\';\n' +
     'import ReactDOM from \'react-dom\';\n' +
-    recast.print(ast).code;
+    code;
 };
