48ee494122b81e6de3aeb9192e819ebaa342d001
Benjy Cui <benjytrys@gmail.com>
2016-07-26 14:16:52 +0800

    feat: update routes usage

--- packages/bisheng-core/lib/entry.nunjucks.js
+++ packages/bisheng-core/lib/entry.nunjucks.js
@@ -5,45 +5,36 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const ReactRouter = require('react-router');
 const NProgress = require('nprogress');
-const Route = ReactRouter.Route;
-const Redirect = ReactRouter.Redirect;
 const history = require('history');
-const toCompletedRoutes = require('./utils/to-completed-routes');
 const templateWrapper = require('./utils/template-wrapper.nunjucks');
 
 const theme = require('{{ themePath }}');
-theme.completedRoutes = toCompletedRoutes(theme.routes);
-function generateReactRouter(item, index) {
-  const children = item.children && item.children.map(generateReactRouter);
-  return React.createElement(Route, Object.assign({}, item.props || {}, {
-    key: `route-${index}`,
-    path: item.route,
-    onEnter: () => NProgress.start(),
-    getComponents: templateWrapper(item.template, item.dataPath || item.route),
-    children,
-  }));
-}
-const routes = theme.completedRoutes.map(generateReactRouter);
+const routes = Array.isArray(theme.routes) ? theme.routes : [theme.routes];
 
-routes.push(React.createElement(Route, {
-  key: 'not-found',
+function processRoutes(route) {
+  if (Array.isArray(route)) {
+    return route.map(processRoutes);
+  }
+
+  return Object.assign({}, route, {
+    onEnter: () => NProgress.start(),
+    component: undefined,
+    getComponents: templateWrapper(route.component, route.dataPath || route.path),
+    childRoutes: route.childRoutes && route.childRoutes.map(processRoutes),
+  });
+}
+
+const processedRoutes = processRoutes(routes);
+processedRoutes.push({
   path: '*',
   getComponents: templateWrapper('./template/NotFound'),
-}));
-
-const configRedirects = theme.redirects || {};
-const redirects = Object.keys(configRedirects)
-        .map((from, index) => React.createElement(Redirect, {
-          key: `redirect-${index}`,
-          from,
-          to: configRedirects[from],
-        }));
+});
 
 const router = React.createElement(ReactRouter.Router, {
   history: ReactRouter.useRouterHistory(history.createHistory)({
     basename: '{{ root }}',
   }),
-  children: redirects.concat(routes),
+  routes: processedRoutes,
 });
 ReactDOM.render(
   router,
--- packages/bisheng-core/test/utils/generate-files-path.test.js
+++ packages/bisheng-core/test/utils/generate-files-path.test.js
@@ -10,18 +10,18 @@ describe('utils/generate-files-path', function() {
   });
 
   it('should add index.html to each directory', function() {
-    const result = generateFilesPath([{ route: '/' }], {});
+    const result = generateFilesPath([{ path: '/' }], {});
     assert.deepEqual(result, ['/index.html', '/404.html']);
   });
 
   it('should generate corresponding html file to each file', function() {
-    const result = generateFilesPath([{ route: '/archive' }], {});
+    const result = generateFilesPath([{ path: '/archive' }], {});
     assert.deepEqual(result, ['/archive.html', '/404.html']);
   });
 
   it('should generate corresponding html file to each markdown data', function() {
     const result = generateFilesPath([{
-      route: '/:post',
+      path: '/:post',
       dataPath: '/:post',
     }], {
       hello: {},
