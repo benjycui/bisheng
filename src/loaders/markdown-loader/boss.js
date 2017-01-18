e7a6631edc76ed471761278856083a14f082e072
Benjy Cui <benjytrys@gmail.com>
2017-01-18 11:25:03 +0800

    refactor: use multi process to parse markdwon

---
+++ src/loaders/markdown-loader/boss.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const os = require('os');
+const path = require('path');
+const childProcess = require('child_process');
+
+function createWorkers(count) {
+  const workers = [];
+  while (workers.length < count) {
+    const worker = childProcess.fork(path.join(__dirname, './worker.js'));
+    worker.setMaxListeners(1);
+    workers.push(worker);
+  }
+  return workers;
+}
+
+const workersCount = os.cpus().length - 1;
+
+module.exports = (function() {
+  const workers = createWorkers(workersCount);
+  const tasksQueue = [];
+  function arrange(task) {
+    const worker = workers.pop();
+    const { callback } = task;
+    worker.send(task);
+    worker.on('message', result => {
+      callback(null, result);
+      workers.push(worker); // mission completed
+      if (tasksQueue.length > 0) {
+        arrange(tasksQueue.pop());
+      }
+    });
+  }
+  return {
+    queue(task) {
+      if (workers.length <= 0) {
+        tasksQueue.push(task);
+        return;
+      }
+      arrange(task);
+    },
+  };
+})();
---
+++ src/loaders/markdown-loader/worker.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const markdownData = require('../../utils/markdown-data');
+const stringify = require('../../utils/stringify');
+
+process.on('message', function(task) {
+  const {
+    filename,
+    content,
+    plugins,
+    isBuild,
+  } = task;
+  const parsedMarkdown = markdownData
+          .process(filename, content, plugins, isBuild);
+  const result = `module.exports = ${stringify(parsedMarkdown)};`;
+  process.send(result);
+});
