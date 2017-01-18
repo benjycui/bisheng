06066ab16fd4f1cc7b3d8a42a90abdb0017f5355
Benjy Cui <benjytrys@gmail.com>
2017-01-18 10:01:30 +0800

    refactor: extract stringify as utils

---
+++ src/utils/stringify.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function stringify(node, depth = 0) {
+  const indent = '  '.repeat(depth);
+  if (Array.isArray(node)) {
+    return `[\n` +
+      node.map(item => `${indent}  ${stringify(item, depth + 1)}`).join(',\n') +
+      `\n${indent}]`;
+  }
+  if (
+    typeof node === 'object' &&
+      node !== null &&
+      !(node instanceof Date)
+  ) {
+    if (node.__BISHENG_EMBEDED_CODE) {
+      return node.code;
+    }
+    return `{\n` +
+      Object.keys(node).map((key) => {
+        const value = node[key];
+        return `${indent}  "${key}": ${stringify(value, depth + 1)}`;
+      }).join(',\n') +
+      `\n${indent}}`;
+  }
+  return JSON.stringify(node, null, 2);
+};
