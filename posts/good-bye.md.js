9e4db6212def24c52f3d8405bdd6f55d689488cd
Benjy Cui <benjytrys@gmail.com>
2017-12-19 16:52:16 +0800

    Updates

--- posts/good-bye.md.js
+++ posts/good-bye.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([1],{481:function(e,o){e.exports={content:["article",{},["p","Good bye!"]],meta:{title:"Good bye!",publishDate:"2015-05-05T00:00:00.000Z",filename:"posts/good-bye.md"},description:["section",["p","The second article which is posted by BiSheng."]]}}});
+webpackJsonp([1],{478:function(e,o){e.exports={content:["article",{},["p","Good bye!"]],meta:{title:"Good bye!",publishDate:"2015-05-05T00:00:00.000Z",filename:"posts/good-bye.md"},description:["section",["p","The second article which is posted by BiSheng."]]}}});
