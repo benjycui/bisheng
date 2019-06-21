ff63a4905c389a3af7c491130a9417e7ff622ea9
zombiej <smith3816@gmail.com>
2019-06-21 15:37:54 +0800

    add ts transform support

--- packages/bisheng-plugin-react/lib/transformer.js
+++ packages/bisheng-plugin-react/lib/transformer.js
@@ -43,7 +43,12 @@ const defaultBabelConfig = {
     ],
   ],
   plugins: [
-    ['@babel/plugin-proposal-decorators',
+    [
+      '@babel/plugin-transform-typescript',
+      { isTSX: true },
+    ],
+    [
+      '@babel/plugin-proposal-decorators',
       { decoratorsBeforeExport: true },
     ],
     '@babel/plugin-proposal-class-properties',
