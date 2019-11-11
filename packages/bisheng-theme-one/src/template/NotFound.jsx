9427b8968ed7fa191b32ab76918d3a25e2f68cb4
ycjcl868 <45808948@qq.com>
2019-11-11 13:17:13 +0800

    chore: use react-helme test

--- packages/bisheng-theme-one/src/template/NotFound.jsx
+++ packages/bisheng-theme-one/src/template/NotFound.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import DocumentTitle from 'react-document-title';
+import { Helmet } from 'react-helmet';
 import Layout from './Layout';
 
 export default (props) => {
   return (
-    <DocumentTitle title="Not Found | BiSheng Theme One">
+    <Helmet>
+      <title>Not Found | BiSheng Theme One</title>
       <Layout {...props}>
         <h1 className="entry-title">404 Not Found!</h1>
       </Layout>
-    </DocumentTitle>
+    </Helmet>
   );
 }
