5e0189f0db0a954c388bb3ffb90117f81c626d7f
ycjcl868 <45808948@qq.com>
2019-11-11 12:28:17 +0800

    feat: support react-helmet-async

--- packages/bisheng-theme-one/src/template/Layout.jsx
+++ packages/bisheng-theme-one/src/template/Layout.jsx
@@ -1,45 +1,48 @@
 import React from 'react';
 import { Link } from 'bisheng/router';
+import { HelmetProvider } from 'react-helmet-async';
 import '../static/style';
 
-export default ({ themeConfig, children }) => {
+export default ({ themeConfig, children, helmetContext = {} }) => {
   return (
-    <div>
-      <div className="header">
-        <div className="container">
-          <div className="brand">
-            <Link className="home" to={themeConfig.home}>{themeConfig.sitename}</Link>
+    <HelmetProvider context={helmetContext}>
+      <div>
+        <div className="header">
+          <div className="container">
+            <div className="brand">
+              <Link className="home" to={themeConfig.home}>{themeConfig.sitename}</Link>
+              {
+                !themeConfig.tagline ? null :
+                  <span>- <span className="tagline">{themeConfig.tagline}</span></span>
+              }
+            </div>
             {
-              !themeConfig.tagline ? null :
-                <span>- <span className="tagline">{themeConfig.tagline}</span></span>
+              !themeConfig.navigation ? null :
+                <div className="menu" role="navigation">
+                  {
+                    themeConfig.navigation.map((item, index) =>
+                      <Link to={item.link} key={index}>{item.title}</Link>
+                    )
+                  }
+                </div>
             }
           </div>
+        </div>
+        <div className="document yue">
+          {children}
+        </div>
+        <div className="footer">
+          {themeConfig.footer ? themeConfig.footer : null}
           {
-            !themeConfig.navigation ? null :
-              <div className="menu" role="navigation">
-                {
-                  themeConfig.navigation.map((item, index) =>
-                    <Link to={item.link} key={index}>{item.title}</Link>
-                  )
-                }
-              </div>
+            themeConfig.hideBisheng ? null :
+              <p className="copyright">powered by <a href="https://github.com/benjycui/bisheng">BiSheng</a></p>
           }
         </div>
-      </div>
-      <div className="document yue">
-        {children}
-      </div>
-      <div className="footer">
-        {themeConfig.footer ? themeConfig.footer : null}
         {
-          themeConfig.hideBisheng ? null :
-            <p className="copyright">powered by <a href="https://github.com/benjycui/bisheng">BiSheng</a></p>
+          !themeConfig.github ? null :
+            <div className="github"><a className="github-link" href={themeConfig.github}>Fork me on GitHub</a></div>
         }
       </div>
-      {
-        !themeConfig.github ? null :
-          <div className="github"><a className="github-link" href={themeConfig.github}>Fork me on GitHub</a></div>
-      }
-    </div>
+    </HelmetProvider>      
   );
 }
